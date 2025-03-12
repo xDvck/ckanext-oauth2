5bed6fb21c064357f2e9299881ee431965359aa7
vvedder <vincent.vedder@igd-r.fraunhofer.de>
2025-03-12 14:20:44 +0100

    Set db bind correctly

--- ckanext/oauth2/db.py
+++ ckanext/oauth2/db.py
@@ -46,4 +46,6 @@ def init_db(model):
         # Create the table only if it does not exist
         user_token_table.create(checkfirst=True)
 
+        user_token_table.create(bind=model.Session.bind, checkfirst=True)
+
         model.meta.mapper(UserToken, user_token_table)
